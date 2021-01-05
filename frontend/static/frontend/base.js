5119edae7968b5363ed2880f6c318d97ef1f8886
Noah Ray Roberts <noahray216@gmail.com>
2021-01-05 08:34:54 -0600

    training styling functionality

--- frontend/static/frontend/base.js
+++ frontend/static/frontend/base.js
@@ -1,10 +1,41 @@
+
+// group items hover styling
+document.querySelectorAll('.group_cont').forEach((item) => {
+    item.addEventListener('mouseover', function() {
+        if (this.style.boxShadow.substring(0, 3) != 'red') {
+            this.style.boxShadow = '0px 0px 8px -2px rgba(143,143,143,1)'
+        }
+    })
+    item.addEventListener('mouseout', function() {
+        if (this.style.boxShadow.substring(0, 3) != 'red') {
+            this.style.boxShadow = 'none'
+        }
+    })
+})
+
+// word count form section appreance
 function nextStep() {
     document.querySelectorAll('.part1').forEach((item) => {
-        item.style.display = 'none'
+        item.style.display = 'none';
     })
     document.querySelectorAll('.part2').forEach((item) => {
-        item.style.display = 'unset'
+        item.style.display = 'unset';
     })
     document.querySelector('#word_amount').select()
+}
+
+// check and uncheck group items
+function check_uncheck(elem) {
+
+    item = document.querySelector(`#cont${elem}`);
+    shadowColor = item.style.boxShadow.substring(0, 3);
+
+    if (shadowColor == 'red') {
+        item.style.boxShadow = 'none';
+        item.querySelector('input').checked = false;
+    } else {
+        item.style.boxShadow = '0px 0px 8px -2px red';
+        item.querySelector('input').checked = true;
+    }
 }
 
